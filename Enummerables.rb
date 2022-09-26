1a08405443401de2192956c38cf93ebc54f73a31
korycfitz <korycfitz@gmail.com>
2022-09-26 17:14:00 -0400

    finished factors, almost finished bubble sort

--- Enummerables.rb
+++ Enummerables.rb
@@ -20,7 +20,33 @@ def factors(num)
     (1..num).select {|factor| num%factor==0}
 end
 
+p factors(11)
+p factors(10)
 
+def subwords(str, arr)
+    arr.select { |word| str.include? word }
+end
 
+dictionary = ["bears", "ear", "a", "army"]
+p subwords("erbearsweatmyajs", dictionary)
 
-# p factors(11)
+def doubler(array)
+    array.map {|ele| ele * 2 }
+end
+
+p doubler([1, 2, 3])
+
+def bubble_sort!(array)
+
+    i = 0
+    while i < array.length - 1
+        next_num = array[i + 1]
+        if array[i] < next_num
+            array[i], array[i+1] = next_num, array[i]
+        end
+        i+=1
+    end
+    array
+end
+
+p bubble_sort!([4, 4, 3, 2, 1, 3, 4, 7, 6])
