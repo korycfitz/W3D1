d51c0fa7230a68b56f8d1bed46ab01d7f2839c06
ishsgit <aishtahad25@gmail.com>
2022-09-26 17:37:23 -0400

    bubble sorts

--- Enummerables.rb
+++ Enummerables.rb
@@ -37,16 +37,54 @@ end
 p doubler([1, 2, 3])
 
 def bubble_sort!(array)
+    # array_1=array.dup
+    sorted=false
 
-    i = 0
-    while i < array.length - 1
-        next_num = array[i + 1]
-        if array[i] < next_num
-            array[i], array[i+1] = next_num, array[i]
+
+    while !sorted
+    sorted=true
+
+        i = 0
+        while i < array.length - 1
+            next_num = array[i + 1]
+            if array[i] < next_num
+                array[i], array[i+1] = next_num, array[i]
+                sorted=false
+            end
+            i+=1
         end
-        i+=1
     end
     array
 end
 
-p bubble_sort!([4, 4, 3, 2, 1, 3, 4, 7, 6])
+def bubble_sort(array)
+    array_1=array.dup
+    p "____________________"
+    p array_1.object_id
+    p "____________________"
+    p array.object_id
+    sorted=false
+
+
+    while !sorted
+    sorted=true
+
+        i = 0
+        while i < array_1.length - 1
+            next_num = array_1[i + 1]
+            if array_1[i] < next_num
+                array_1[i], next_num = next_num, array_1[i]
+                sorted=false
+            end
+            i+=1
+        end
+    end
+    array_1
+    p "____________________"
+    p array_1.object_id
+    p "____________________"
+    p array.object_id
+   
+end
+
+p bubble_sort([4, 4, 3, 2, 1, 3, 4, 7, 6])
